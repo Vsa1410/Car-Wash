b8fbb120cf2c744ef7c6c550ead52693f11ee40d
Vinicius Silva Alves <vsa1410@hotmail.com>
2022-07-12 23:29:10 -0300

    Layout Updates

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
 import Home from './components/home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header className="header"/>
+      
+      <Home className="home"/>
+      <Home className="home"/>
       <Home className="home"/>
       <Footer className="footer"/>
     </div>
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,8 +4,23 @@ import './index.css'
 function Home() {
     return(
         <div className="home">
-            <p style={{marginTop: "120px"}}>Oiii</p>
-            
+            <div className="lastService" style={{display:"flex", justifyContent:"space-around"}}>
+                <div className="lastService1">
+                    <p style={{color: "rgb(54, 3, 13)", fontSize: "10px",margin:0}}>Lavagem</p>
+                    <h3 style={{margin:"0"}}>Vinicius Alves</h3>
+                    <p style={{color: "rgb(54, 3, 13)", fontSize: "10px",margin:"10px 0 0 0"}}>Data</p>
+                    <h4 style={{margin:0}}>12/07/2022</h4>
+                </div>
+                <div>
+                    <p style={{color: "rgb(54, 3, 13)", fontSize: "10px",margin:0}}>Valor</p>
+                    <h3 style={{margin:"0"}}>R$ 20,00</h3>
+                    <p style={{color: "rgb(54, 3, 13)", fontSize: "10px",margin:"10px 0 0 0"}}>Cliente</p>
+                    <h5 style={{margin:0}}>Jefferson</h5>
+
+                </div>           
+
+            </div>
+
         </div>
     )
 }
