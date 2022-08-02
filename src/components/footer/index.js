d3056567edcf673aeca8fb75df64556511e649df
Vinicius Silva Alves <vsa1410@hotmail.com>
2022-08-01 23:18:00 -0300

    Updates on footer and implementation of MUI at that

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,41 +1,59 @@
-import React from "react";
+
 import './index.css'
 import Home from "../home";
 import { Link } from "react-router-dom";
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import HomeIcon from '@mui/icons-material/Home';
+import LocalCarWashIcon from '@mui/icons-material/LocalCarWash';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import EngineeringIcon from '@mui/icons-material/Engineering';
+import Box from '@mui/material/Box';
 
 function Footer (){
+    const[value,setValue]= React.useState(0)
     return(
         <div className="footer">
-            <Link to={"/"}>
-            <span class="material-symbols-outlined" style={{cursor:"pointer"}}>
-            home
-            </span>
-            </Link>
+        <Box sx={{width: 400}}>
 
-            <Link to={"/payments"}>
-            <span class="material-symbols-outlined" style={{cursor:"pointer"}}>
-            payments
-            </span>
-            </Link>
-
-            <Link to={"/new"}>
-            <span class="material-symbols-outlined" style={{cursor:"pointer"}}>
-            add
-            </span>
-            </Link>
-
-            <Link to={"/saldo"}>
-            <span class="material-symbols-outlined" style={{cursor:"pointer"}}>
-            done
-            </span>
-            </Link>
+            <BottomNavigation
+                showLabels
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+                >
             
-            <span class="material-symbols-outlined" style={{cursor:"pointer"}}>
-            person
-            </span>
+           
+                <BottomNavigationAction component={Link} to="/"label="Home" icon={<HomeIcon />} />
+            
 
 
 
+            
+            
+                <BottomNavigationAction component={Link} to="/payments" label="Lançamentos" icon={<LocalCarWashIcon />} />
+            
+
+            
+                <BottomNavigationAction component={Link} to="/new" label="Adicionar" icon={<AddCircleIcon />} />
+            
+            
+
+            
+            <BottomNavigationAction component={Link} to="/saldo" label="Saldo" icon={<AccountBalanceIcon />} />
+            
+            
+            <BottomNavigationAction label="Manutenção" icon={<EngineeringIcon />} />
+
+            </BottomNavigation>
+
+
+                    </Box>
 
         </div>
     )
