9e839eeee36ee4ed253d48c683d8f9d2ad7c7bd2
Vinicius Silva Alves <vsa1410@hotmail.com>
2022-08-07 11:19:02 -0300

    changed the url to work locally only, due the cors does not work

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -4,7 +4,7 @@ import Payments from "../payments";
 import AddNew from "../addNew";
 import { Link } from "react-router-dom";
 
-export const url = "https://car-wash-back.herokuapp.com"
+export const url = "http://192.168.2.104:3000"
 function Home() {
     
    
